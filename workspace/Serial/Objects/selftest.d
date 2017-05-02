100ed21604f16e47476477f15340815e5f0374c0
Matthias Kraus <matthiaskraus@live.de>
2017-05-02 15:22:51 +0200

    Serial: In Keil Projekt umwandeln

---
+++ workspace/Serial/Objects/selftest.d
@@ -0,0 +1,4 @@
+.\objects\selftest.o: src\selftest.c
+.\objects\selftest.o: src\selftest.h
+.\objects\selftest.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdio.h
+.\objects\selftest.o: src\main.h
---
+++ workspace/Serial/Objects/tm_stm32f4_usart.d
@@ -0,0 +1,2 @@
+.\objects\tm_stm32f4_usart.o: Libraries\USART\tm_stm32f4_usart.c
+.\objects\tm_stm32f4_usart.o: Libraries\USART\tm_stm32f4_usart.h
