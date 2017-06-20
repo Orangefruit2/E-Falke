fd780b3a4c742a7b67df8d3fdbe2266c96bacbcc
Matthias Kraus <matthiaskraus@live.de>
2017-06-20 15:45:23 +0200

    Display aus main entfernt

--- workspace/EFalkeController/main.h
+++ workspace/EFalkeController/main.h
@@ -5,15 +5,12 @@
 #include "clock.h"
 #include "watchdog.h"
 #include "Serial/Serial.h"
-<<<<<<< HEAD
-
-//IWDG_HandleTypeDef hiwdg;		// Watchdog
 
 
 void rxCallback(uint8_t uart, pi300_control_msg* msg);
 
-#endif
-=======
 #include "Display/Display.h"
+
+
 #endif
->>>>>>> Display
+
